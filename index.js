ae9199caf59aadfeaacf92b5c070c132aaf454b3
Ifeoluwa <ifeolufaro@gmail.com>
2022-06-24 08:48:07 +0100

    Add file

---
+++ index.js
@@ -0,0 +1,10 @@
+const name = "Faromika Ifeoluwa Faith";
+
+const height = 9;
+
+const country = 'Nigeria';
+
+
+console.log("My name is " + name);
+console.log("My height is " + height + "m.");
+console.log("My country is " + country);
