77d02d6c4e48f99fed1da72d8dd04ecbccf413e0
Ifeoluwa <ifeolufaro@gmail.com>
2022-06-24 09:29:10 +0100

    Third commit

--- index.js
+++ index.js
@@ -12,4 +12,4 @@ console.log("My country is " + country);
 const body = document.querySelector('body');
 
 
-body.innerHTML = `<div><h1>My name is ${name}</h1><p>My height is ${height}</p>My country is ${country}<p></p></div>`;
+body.innerHTML = `<div><h1>My name is ${name}.</h1><p>My height is ${height}m.</p>My country is ${country}.<p></p></div>`;
